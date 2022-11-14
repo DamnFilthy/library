d441d25bed64c98125891c27dc7dc8ccef22089c
Alexander <aleksandr-shiryaev-1993@mail.ru>
2022-11-14 12:43:08 +0300

    update

--- express-library/index.js
+++ express-library/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const logger = require('./middleware/logger')
 const error404 = require('./middleware/error404')
 const {apiRouter} = require('./routes/apiRouter')
 const publicRouter = require('./routes/publicRouter')
+const cors = require('cors');
+const clientUrl = 'http://localhost:8083';
 
 app = express()
 app.set('view engine', 'ejs')
+
+app.use(cors({origin: '*'}));
+app.all('/*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', clientUrl);
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+});
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -16,6 +26,26 @@ app.use('/site', publicRouter)
 app.use(error404)
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT)
+const urlDB = process.env.urlDB
+const LOCAL_DB_URL = process.env.LOCAL_DB_URL
+const local_url = 'mongodb://root:example@mongo:27017/books?authSource=admin'
+const test_url = 'mongodb://root:example@mongo:27017/books/streamhatchet?directConnection=true&authSource=admin&replicaSet=replicaset&retryWrites=true'
+
+const options = {
+        user: "admin",
+        pass: "pass",
+        dbName: 'books'
+    }
+
+;(async () => {
+    try {
+        await mongoose.connect('mongodb://mongo:27017', options)
+            .then(() => console.log("Database connected!"))
+            .catch(err => console.log(err));
+        app.listen(PORT)
+    } catch (e) {
+        console.log(e)
+    }
+})()
 
 module.exports = app
--- express-library/routes/publicRouter.js
+++ express-library/routes/publicRouter.js
@@ -1,69 +1,86 @@
+const mongoose = require('mongoose');
 const express = require('express')
 const publicRouter = express.Router()
-const {store} = require('./apiRouter')
 const request = require('request');
 const COUNTER_URL = process.env.COUNTER_URL || 'http://counter:3002/'
 
-publicRouter.get('/books', (req, res) => {
-    const {books} = store
-    res.status(200)
-    res.render('book/index', {
-        title: 'Books',
-        pageTitle: 'Список Книг',
-        books
-    })
-})
+const Book = require('../models/book')
 
-publicRouter.get('/books/:id', async (req, res) => {
-    const {books} = store
-    const {id} = req.params
-    const index = books.findIndex(item => item.id === id)
+let ObjectId = require('mongodb').ObjectId;
 
-    if (index !== -1) {
-        await request.post(
-            COUNTER_URL + '/counter/' + id + '/incr',
-            function (error) {
-                if (error) {
-                    res.status(404)
-                    res.json({error: error})
-                }
-            })
-        await request.get(COUNTER_URL + '/counter/' + id,
-            function (error, response) {
-                if (!error) {
-                    let jsonBody = JSON.parse(response.body)
-                    console.log(response.body)
-
-                    res.status(200)
-                    res.render('book/view', {
-                        title: books[index].title,
-                        pageTitle: books[index].title,
-                        book: books[index],
-                        counter: jsonBody.counter
-                    })
-                } else {
-                    res.status(404)
-                    res.json({error: error})
-                }
-            }
-        )
-    } else {
-        res.status(404)
-        res.json({error: '404 | книга не найдена'})
+publicRouter.get('/books', async (req, res) => {
+    try {
+        const books = await Book.find({}).select('-__v')
+        res.status(200)
+        res.render('book/index', {
+            title: 'Books',
+            pageTitle: 'Список Книг',
+            books
+        })
+    } catch (e) {
+        console.log(e)
+        res.status(500).json(e)
     }
 })
 
-publicRouter.get('/books/:id/update', (req, res) => {
-    const {books} = store
+publicRouter.get('/books/:id', async (req, res) => {
     const {id} = req.params
-    const index = books.findIndex(item => item.id === id)
+    let o_id = new ObjectId(id);
 
-    if (index !== -1) {
+    const bookID = await Book.findOne({"_id": o_id})
+
+    try {
+        if (bookID) {
+            await request.post(
+                COUNTER_URL + '/counter/' + o_id + '/incr',
+                function (error) {
+                    if (error) {
+                        res.status(404)
+                        res.json({error: error})
+                    }
+                })
+            await request.get(COUNTER_URL + '/counter/' + o_id,
+                function (error, response) {
+                    if (!error) {
+                        let jsonBody = JSON.parse(response.body)
+                        console.log(response.body)
+
+                        res.status(200)
+                        res.render('book/view', {
+                            title: bookID.title,
+                            pageTitle: bookID.title,
+                            book: bookID,
+                            counter: jsonBody.counter
+                        })
+                    } else {
+                        res.status(404)
+                        res.json({error: error})
+                    }
+                }
+            )
+        } else {
+            res.status(404)
+            res.json({error: '404 | книга не найдена'})
+        }
+    } catch (e) {
+        console.log(e)
+        res.status(404)
+        res.json({error: e})
+    }
+})
+
+publicRouter.get('/books/:id/update', async (req, res) => {
+    const {id} = req.params
+    let o_id = new ObjectId(id);
+
+    const bookID = await Book.findOne({"_id": o_id})
+
+    if (bookID) {
         res.status(200)
         res.render('book/update', {
-            title: books[index].title,
-            pageTitle: books[index].title,
-            book: books[index]
+            title: bookID.title,
+            pageTitle: bookID.title,
+            book: bookID
         })
     } else {
         res.status(404)
